02dc4e52ac3aed98424b426f3f0ef6a1c334c3c3
Elijah Carrel <ecarrel@grailbio.com>
2019-04-23 23:45:21 -0700

    i think this does work, testing again

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import Link from 'next/link'
 import Layout from '../common-components/Layout/Layout'
+import { CommonLink } from "../common-components/CommonLink/CommonLink";
 
 const AboutPage: React.FunctionComponent = () => (
   <Layout title="About | Next.js + TypeScript Example">
     <h1>About</h1>
-    <p>This is the about page</p>
+    <p>This is Inbox Comics v2 prototype. Go to <CommonLink href="http://www.inboxcomics.com/">real site</CommonLink></p>
     <p><Link href='/'><a>Go home</a></Link></p>
   </Layout>
 )
