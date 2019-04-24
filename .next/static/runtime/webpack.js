84f15d806b262d3d7f852be1681d880236ef9df8
Elijah Carrel <elijahcarrel@gmail.com>
2019-04-23 23:48:27 -0700

    not sure

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,11 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
+<<<<<<< HEAD
 /******/ 	var hotCurrentHash = "fbc038d64b47205d489e";
+=======
+/******/ 	var hotCurrentHash = "a3cbb1875552e212dcc1";
+>>>>>>> not sure
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
