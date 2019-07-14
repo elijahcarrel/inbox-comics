d6ae6adf457cf4ef9097b1049feabd5ca4d62259
Gabe <greiss122@gmail.com>
2019-07-13 20:50:06 -0700

    contact page updates

--- client/pages/contact.tsx
+++ client/pages/contact.tsx
@@ -1,13 +1,23 @@
 import * as React from "react";
 import { Layout } from "../common-components/Layout/Layout";
+import { CommonLink } from "../common-components/CommonLink/CommonLink";
 import { Title } from "../common-components/Title/Title";
+import { TextInput } from "../common-components/TextInput/TextInput";
+import styles from "./contact.module.scss";
+
 
 const ContactPage: React.FunctionComponent = () => (
   <Layout title="Contact">
     <Title>Contact</Title>
-    <p>
-      Haven't built this page yet.
-    </p>
+    <div className={styles.contactForm}>
+      <p class="contacttext">We'd love to hear from you!</p>
+      <p class="contacttext">You can also reach us by replying to any of your daily comic emails, or by emailing us at  <CommonLink lowercase href="mailto:hello@inboxcomics.com">hello@inboxcomics.com</CommonLink>.</p>
+      <TextInput name="Name"/> < br/>
+      <TextInput name="Email Address"/> < br/>
+      <TextInput name="Subject"/> < br/>
+      <TextInput name="Message"/> < br/> 
+      <input type="submit" />         
+    </div>
   </Layout>
 );
 
