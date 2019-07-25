3c85c6a389e7c6a61599d02f3d218a59f64d0bca
Elijah Carrel <ecarrel@grailbio.com>
2019-07-24 23:31:50 -0700

    no underline on logo

--- client/common-components/CommonLink/CommonLink.tsx
+++ client/common-components/CommonLink/CommonLink.tsx
@@ -7,15 +7,19 @@ interface Props {
   href: string;
   children: React.ReactNode;
   lowercase?: boolean;
+  underline?: boolean;
 }
 
 export const CommonLink: React.FunctionComponent<Props> = (props: Props) => {
-  const { href, children, lowercase = false } = props;
+  const { href, children, lowercase = false, underline = true } = props;
   return (
     <Link
       href={href}
     >
-      <a className={classNames(styles.link, {[styles.uppercase]: !lowercase})}>{children}</a>
+      <a className={classNames(styles.link,
+        {[styles.uppercase]: !lowercase},
+        {[styles.noUnderline]: !underline},
+      )}>{children}</a>
     </Link>
   );
 };
