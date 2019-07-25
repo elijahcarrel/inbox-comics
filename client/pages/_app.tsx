673165c9b8f91df76678642767b97b5941f47fb0
Elijah Carrel <elijahcarrel@gmail.com>
2019-07-25 07:32:53 -0700

    Fix link bug

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { ApolloClient, NormalizedCacheObject } from "apollo-boost";
 import App, { Container } from "next/app";
+import Router from "next/router";
 import React from "react";
 import { ApolloProvider } from "react-apollo";
 import { withApolloClient } from "../lib/with-apollo-client";
+
+// Workaround to fix https://github.com/zeit/next-plugins/issues/282.
+Router.events.on("routeChangeComplete", () => {
+  if (process.env.NODE_ENV !== "production") {
+    const els = document.querySelectorAll('link[href*="/_next/static/css/styles.chunk.css"]');
+    const timestamp = new Date().valueOf();
+    // @ts-ignore href does not exist on element.
+    els[0].href = "/_next/static/css/styles.chunk.css?v=" + timestamp;
+  }
+});
 
 interface Props {
   apolloClient: ApolloClient<NormalizedCacheObject>;
