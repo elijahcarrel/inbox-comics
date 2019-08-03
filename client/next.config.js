c20b391df8bb987f16b4eed376bf3b9387b4d175
Elijah Carrel <elijahcarrel@gmail.com>
2019-08-03 00:59:02 -0700

    Fix css

--- client/next.config.js
+++ client/next.config.js
@@ -1,43 +1,10 @@
 const withSass = require('@zeit/next-sass');
-const withCss = require('@zeit/next-css');
-module.exports = withCss(
-  withSass({
-    webpack (config, options) {
-      config.module.rules.forEach(rule => {
-        if (rule.test.toString().includes('.scss')) {
-          
-          rule.rules = rule.use.map(useRule => {
-            if (typeof useRule === 'string') {
-              return {
-                loader: useRule,
-              };
-            }
-            if (useRule.loader.startsWith('css-loader')) {
-              return {
-                oneOf: [
-                  {
-                    test: /\.global\.scss$/,
-                    loader: useRule.loader,
-                    options: {
-                      ...useRule.options,
-                      modules: false,
-                    },
-                  },
-                  {
-                    loader: useRule.loader,
-                    options: useRule.options,
-                  },
-                ],
-              };
-            }
-        
-            return useRule;
-          });
-      
-          delete rule.use;
-        }
-      });
-      return config;
-    }
-  })
-);
+module.exports = withSass({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]___[hash:base64:5]",
+  },
+  target: 'serverless',
+  crossOrigin: 'anonymous',
+});
