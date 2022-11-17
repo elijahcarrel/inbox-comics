2c44cdadc278908aa1a81d5d83cf8f51db58b255
Elijah Carrel <elijahcarrel@gmail.com>
2022-11-16 20:12:55 -0800

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,9 @@ import {
 import fetch from "isomorphic-unfetch";
 import App from "next/app";
 import React from "react";
+// TODO(ecarrel): remove the eslint-disable directive once https://github.com/vercel/analytics/issues/18 is resolved.
+// eslint-disable-next-line import/no-unresolved
+import { Analytics } from "@vercel/analytics/react";
 import { ToasterContainer } from "../common-components/ToasterContainer/ToasterContainer";
 
 const graphQLHttpEndpoint = "/api/graphql";
@@ -27,6 +30,7 @@ class MyApp extends App {
     return (
       <ApolloProvider client={client}>
         <Component {...pageProps} />
+        <Analytics />
         <ToasterContainer />
       </ApolloProvider>
     );
