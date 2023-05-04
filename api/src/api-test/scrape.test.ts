6f839201e05779ac7ed800eecb942375215fb2fa
Elijah Carrel <elijahcarrel@gmail.com>
2023-05-03 22:15:44 -0700

    add one more unit test for scraping (#91)

--- api/src/api-test/scrape.test.ts
+++ api/src/api-test/scrape.test.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 import { Syndication } from "../db-models/comic-syndication";
 import { assertIsDefined } from "../util/ts";
-import { scrapeComicForSyndication } from "../service/scrape";
+import {
+  scrapeAndSaveComicForSyndication,
+  scrapeComicForSyndication,
+} from "../service/scrape";
 import { now } from "../util/date";
 import { MOCK_SITE_ID } from "../service/scraper/sites";
 
@@ -45,6 +48,44 @@ it("should scrape comic for syndication", async () => {
     success: true,
     imageUrl: "mock-image-url",
     failureMode: null,
-    imageCaption: null,
+    imageCaption: "mock-image-caption",
   });
+});
+
+it("should scrape and save comic for syndication", async () => {
+  const syndicationIdentifier = "test-syndication";
+
+  const syndication = await Syndication.create({
+    site_id: MOCK_SITE_ID,
+    title: syndicationIdentifier,
+    identifier: syndicationIdentifier,
+    theiridentifier: syndicationIdentifier,
+    numSubscribers: 0,
+  });
+
+  const date = now();
+  const dateIso = date.toISOString();
+  await scrapeAndSaveComicForSyndication(syndication, date);
+
+  const updatedSyndication = await Syndication.findOne({
+    identifier: syndicationIdentifier,
+  });
+  assertIsDefined(updatedSyndication);
+  await updatedSyndication.populate({ path: "lastSuccessfulComic" });
+
+  expect(updatedSyndication.identifier).toEqual(syndicationIdentifier);
+  assertIsDefined(updatedSyndication.lastAttemptedComicScrapeDate);
+  expect(updatedSyndication.lastAttemptedComicScrapeDate.toISOString()).toEqual(
+    dateIso,
+  );
+  assertIsDefined(updatedSyndication.lastSuccessfulComicScrapeDate);
+  expect(
+    updatedSyndication.lastSuccessfulComicScrapeDate.toISOString(),
+  ).toEqual(dateIso);
+  expect(updatedSyndication.lastSuccessfulComic.imageUrl).toEqual(
+    "mock-image-url",
+  );
+  expect(updatedSyndication.lastSuccessfulComic.imageCaption).toEqual(
+    "mock-image-caption",
+  );
 });
