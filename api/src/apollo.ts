85703630d3866097bd90fae61ffa881193fe6339
Elijah Carrel <elijahcarrel@gmail.com>
2023-05-07 16:06:42 -0700

    disable cross-site request forgery protection

--- api/src/apollo.ts
+++ api/src/apollo.ts
@@ -25,6 +25,8 @@ export const initApollo = (): ((_req: any, _res: any) => Promise<unknown>) => {
         },
       },
     ],
+    // TODO(ecarrel): re-enable once we figure out how to get all clients to pass the right headers.
+    csrfPrevention: false,
   });
 
   return startServerAndCreateNextHandler(apolloServer);
