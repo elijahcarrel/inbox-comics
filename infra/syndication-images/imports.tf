39df7499f23025c8b65c2a5ff11ea21737704696
Elijah Carrel <elijahcarrel@gmail.com>
2023-09-09 23:05:44 -0700

    add terraform for syndication-images bucket

---
+++ infra/syndication-images/imports.tf
@@ -0,0 +1,4 @@
+import {
+  to = aws_s3_bucket.syndication_images
+  id = "syndication-images"
+}
---
+++ infra/syndication-images/syndication-images.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.16"
+    }
+  }
+
+  required_version = ">= 1.5.7"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "syndication_images" {
+  bucket = "syndication-images"
+}
