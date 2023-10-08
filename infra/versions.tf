b71714bc4a5038dba3add35cf72582292ed5de5e
Elijah Carrel <elijahcarrel@gmail.com>
2023-10-08 15:05:10 -0700

    oops

--- infra/versions.tf
+++ infra/versions.tf
@@ -6,9 +6,18 @@ terraform {
     }
   }
 
+  required_providers {
+    mongodbatlas = {
+      source = "mongodb/mongodbatlas"
+      version = "1.12.1"
+    }
+  }
+
   required_version = ">= 1.5.7"
 }
 
 provider "aws" {
   region = "us-west-2"
 }
+
+provider "mongodbatlas" {}
