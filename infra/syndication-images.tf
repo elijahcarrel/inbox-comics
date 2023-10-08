42079d0379ea5c108d0799e1c466c804305e840b
Elijah Carrel <elijahcarrel@gmail.com>
2023-10-08 14:25:30 -0500

    be more specific in terraform s3 bucket configuration (#203)

--- infra/syndication-images.tf
+++ infra/syndication-images.tf
@@ -1,3 +1,76 @@
 resource "aws_s3_bucket" "syndication_images" {
   bucket = "syndication-images"
 }
+
+resource "aws_s3_bucket_cors_configuration" "syndication_images" {
+  bucket = aws_s3_bucket.syndication_images.id
+  cors_rule {
+    allowed_headers = [
+      "*"
+    ]
+    allowed_methods = [
+      "PUT",
+      "POST",
+      "DELETE"
+    ]
+    allowed_origins = [
+      "https://*.retool.com"
+    ]
+    expose_headers = []
+    max_age_seconds = 0
+  }
+
+  cors_rule {
+    allowed_headers = []
+    allowed_methods = [
+      "GET"
+    ]
+    allowed_origins = [
+      "https://*.retool.com",
+      "https://*.vercel.com",
+      "https://*.inboxcomics.com"
+    ]
+    expose_headers = []
+    max_age_seconds = 0
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "syndication_images" {
+  bucket = aws_s3_bucket.syndication_images.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = ""
+      sse_algorithm     = "AES256"
+    }
+
+    bucket_key_enabled = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "syndication_images_general_policy" {
+  bucket = aws_s3_bucket.syndication_images.id
+  policy = data.aws_iam_policy_document.syndication_images_general_policy.json
+}
+
+data "aws_iam_policy_document" "syndication_images_general_policy" {
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = ["*"]
+    }
+
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    sid = "PublicReadGetObject"
+
+    resources = [
+      "${aws_s3_bucket.syndication_images.arn}/*",
+    ]
+
+  }
+}
