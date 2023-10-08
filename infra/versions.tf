5da039f2774be26dc511b5e2a546ffc87c15be2f
Elijah Carrel <elijahcarrel@gmail.com>
2023-10-08 15:06:00 -0700

    fix version blocks

--- infra/versions.tf
+++ infra/versions.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.16"
+      version = ">= 5.16"
     }
   }
 
   required_providers {
     mongodbatlas = {
       source = "mongodb/mongodbatlas"
-      version = "1.12.1"
+      version = ">= 1.12.1"
     }
   }
 
