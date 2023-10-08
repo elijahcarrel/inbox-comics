b6e878a8f7238aafde8295df7e34bc38b4a85803
Elijah Carrel <elijahcarrel@gmail.com>
2023-10-08 15:12:44 -0700

    minor cleanup

--- infra/mongodb.tf
+++ infra/mongodb.tf
@@ -3,7 +3,6 @@ resource "mongodbatlas_cluster" "inbox_comics" {
   name = local.mongodb_cluster_name[terraform.workspace]
   cluster_type = "REPLICASET"
 
-  # Provider Settings "block"
   provider_name = "TENANT"
   backing_provider_name = "AWS"
   provider_region_name = "US_WEST_2"
