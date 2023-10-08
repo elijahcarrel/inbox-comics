4425e5500400268e0f5e1d1a1096a3e347e23338
Elijah Carrel <elijahcarrel@gmail.com>
2023-10-08 13:10:42 -0700

    add variable for s3 bucket name

--- infra/imports.tf
+++ infra/imports.tf
@@ -1,19 +1,19 @@
 import {
   to = aws_s3_bucket.syndication_images
-  id = "syndication-images"
+  id = local.syndication_images_bucket_name[terraform.workspace]
 }
 
 import {
   to = aws_s3_bucket_cors_configuration.syndication_images
-  id = "syndication-images"
+  id = local.syndication_images_bucket_name[terraform.workspace]
 }
 
 import {
   to = aws_s3_bucket_server_side_encryption_configuration.syndication_images
-  id = "syndication-images"
+  id = local.syndication_images_bucket_name[terraform.workspace]
 }
 
 import {
   to = aws_s3_bucket_policy.syndication_images_general_policy
-  id = "syndication-images"
+  id = local.syndication_images_bucket_name[terraform.workspace]
 }
---
+++ infra/variables.tf
@@ -0,0 +1,6 @@
+locals {
+  syndication_images_bucket_name = {
+    inbox-comics-staging = "syndication-images-staging"
+    inbox-comics-prod    = "syndication-images"
+  }
+}
