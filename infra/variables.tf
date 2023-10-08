e6c870e348efb65d893f46ef9c635a1c468ea0e5
Elijah Carrel <elijahcarrel@gmail.com>
2023-10-08 13:13:38 -0700

    cleanup bucket name

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,6 +1,6 @@
 locals {
   syndication_images_bucket_name = {
-    inbox-comics-staging = "syndication-images-staging"
+    inbox-comics-staging = "inbox-comics-syndication-images-staging"
     inbox-comics-prod    = "syndication-images"
   }
 }
