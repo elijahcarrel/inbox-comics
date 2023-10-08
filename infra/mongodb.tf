6db5e75ba35157f17c5e6e9543163157f816c63f
Elijah Carrel <elijahcarrel@gmail.com>
2023-10-08 15:03:38 -0700

    add mongodb cluster

---
+++ infra/mongodb.tf
@@ -0,0 +1,11 @@
+resource "mongodbatlas_cluster" "inbox_comics" {
+  project_id = local.mongodb_project_name[terraform.workspace]
+  name = local.mongodb_cluster_name[terraform.workspace]
+  cluster_type = "REPLICASET"
+
+  # Provider Settings "block"
+  provider_name = "TENANT"
+  backing_provider_name = "AWS"
+  provider_region_name = "US_WEST_2"
+  provider_instance_size_name = "M0"
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,6 +1,17 @@
+# Maps are generally from terraform cloud workspace name to value.
 locals {
   syndication_images_bucket_name = {
     inbox-comics-staging = "inbox-comics-syndication-images-staging"
     inbox-comics-prod    = "syndication-images"
+  }
+
+  mongodb_project_name = {
+    inbox-comics-staging = "inbox-comics-staging"
+    inbox-comics-prod    = "inbox-comics-prod"
+  }
+
+  mongodb_cluster_name = {
+    inbox-comics-staging = "inbox-comics-staging-0"
+    inbox-comics-prod    = "inboxcomics0" # Named this way for historical reasons. Difficult to rename.
   }
 }
