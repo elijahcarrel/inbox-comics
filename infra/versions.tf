340366e1835c43af05a146d10995f782779e0467
Elijah Carrel <elijahcarrel@gmail.com>
2023-10-08 15:06:58 -0700

    ope

--- infra/versions.tf
+++ infra/versions.tf
@@ -4,9 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.16"
     }
-  }
 
-  required_providers {
     mongodbatlas = {
       source = "mongodb/mongodbatlas"
       version = ">= 1.12.1"
