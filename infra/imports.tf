2d1f3af02893fec0617aa6f18ae1192a827ceeb2
Elijah Carrel <elijahcarrel@gmail.com>
2023-10-23 21:23:47 -0700

    add ses config (#208)
    * add ses config
    
    * add imports
    
    * fix common message configuration

--- infra/imports.tf
+++ infra/imports.tf
@@ -2,3 +2,22 @@ import {
   to = mongodbatlas_cluster.inbox_comics
   id = "${local.mongodb_project_id[terraform.workspace]}-${local.mongodb_cluster_name[terraform.workspace]}"
 }
+import {
+  to = aws_ses_email_identity.sender
+  id = "comics@inboxcomics.com"
+}
+
+import {
+  to = aws_ses_email_identity.bounces
+  id = "inboxcomics+bounces@gmail.com"
+}
+
+import {
+  to = aws_ses_domain_identity.example
+  id = "inboxcomics.com"
+}
+
+import {
+  to = aws_ses_configuration_set.common_message_configuration
+  id = "CommonMessageConfiguration"
+}
---
+++ infra/ses.tf
@@ -0,0 +1,19 @@
+resource "aws_ses_email_identity" "sender" {
+  email = "comics@inboxcomics.com"
+}
+
+resource "aws_ses_email_identity" "bounces" {
+  email = "inboxcomics+bounces@gmail.com"
+}
+
+resource "aws_ses_domain_identity" "example" {
+  domain = "inboxcomics.com"
+}
+
+resource "aws_ses_configuration_set" "common_message_configuration" {
+  name = "CommonMessageConfiguration"
+  reputation_metrics_enabled = true
+  delivery_options {
+    tls_policy = "Optional"
+  }
+}
