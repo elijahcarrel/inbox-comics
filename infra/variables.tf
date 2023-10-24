951884ae75eb75becdb09153336db3e124d51433
Elijah Carrel <elijahcarrel@gmail.com>
2023-10-23 23:08:53 -0700

    Split ses terraform per environment (#217)
    Splits SES terraform per environment, thus duplicating all resources for redundancy.

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,10 @@
 # Maps are generally from terraform cloud workspace name to value.
 locals {
+
+  environment = {
+    inbox-comics-staging = "staging"
+    inbox-comics-prod    = "prod"
+  }
   syndication_images_bucket_name = {
     inbox-comics-staging = "inbox-comics-syndication-images-staging"
     inbox-comics-prod    = "syndication-images"
@@ -18,5 +23,20 @@ locals {
   mongodb_cluster_name = {
     inbox-comics-staging = "inbox-comics-staging-0"
     inbox-comics-prod    = "inboxcomics0" # Named this way for historical reasons. Difficult to rename.
+  }
+
+  comics_email_sender = {
+    inbox-comics-staging = "comics-staging@inboxcomics.com"
+    inbox-comics-prod    = "comics@inboxcomics.com"
+  }
+
+  bounce_email_recipient = {
+    inbox-comics-staging = "inboxcomics+bounces-staging@gmail.com"
+    inbox-comics-prod    = "inboxcomics+bounces@gmail.com"
+  }
+
+  domain = {
+    inbox-comics-staging = "app-inboxcomics.vercel.app"
+    inbox-comics-prod    = "inboxcomics.com"
   }
 }
