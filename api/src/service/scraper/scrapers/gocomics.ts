ee36a99bbd4d8b219886ea5ed56515068c0429da
Elijah Carrel <elijahcarrel@gmail.com>
2025-04-11 21:57:33 -0400

    fix gocomics scraper (#399)

--- api/src/service/scraper/scrapers/gocomics.ts
+++ api/src/service/scraper/scrapers/gocomics.ts
@@ -12,17 +12,16 @@ import {
 import { Scraper } from "../scraper";
 
 export class GoComicsScraper extends Scraper {
-  // eslint-disable-next-line class-methods-use-this
-  async scrape(date: Moment, syndication: ISyndication): Promise<ScrapeResult> {
+  async scrape(_: Moment, syndication: ISyndication): Promise<ScrapeResult> {
     const { theiridentifier: theirIdentifier } = syndication;
-    const url = `http://www.gocomics.com/${theirIdentifier}/${date.format(
-      "YYYY/MM/DD",
-    )}`;
+    const url = `https://www.gocomics.com/${theirIdentifier}`;
     const $ = await cheerioRequest(url);
     if ($ === null) {
       return scrapeFailure(failureModes.GOCOMICS_REJECTION);
     }
-    const comicImages = $("picture.item-comic-image img");
+    const comicImages = $(
+      "section[data-sentry-component='ShowComicViewer'] button[aria-label='Expand comic'] img",
+    );
     if (comicImages.length !== 1) {
       return scrapeFailure(failureModes.GOCOMICS_MISSING_IMAGE_ON_PAGE);
     }
