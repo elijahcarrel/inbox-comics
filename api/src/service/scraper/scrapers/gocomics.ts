decc2daab122e9a3adb2f2106236ba8058cecc98
Elijah Carrel <elijahcarrel@gmail.com>
2025-06-09 22:47:47 -0400

    fix gocomics yet again (#419)
    * fix gocomics yet again
    
    * cleanup
    
    * fix type error that weirdly only appears when running jest tests

--- api/src/service/scraper/scrapers/gocomics.ts
+++ api/src/service/scraper/scrapers/gocomics.ts
@@ -12,19 +12,52 @@ import {
 import { Scraper } from "../scraper";
 
 export class GoComicsScraper extends Scraper {
-  async scrape(_: Moment, syndication: ISyndication): Promise<ScrapeResult> {
+  async scrape(date: Moment, syndication: ISyndication): Promise<ScrapeResult> {
     const { theiridentifier: theirIdentifier } = syndication;
     const url = `https://www.gocomics.com/${theirIdentifier}`;
     const $ = await cheerioRequest(url);
     if ($ === null) {
       return scrapeFailure(failureModes.GOCOMICS_REJECTION);
     }
-    const comicImages = $(
-      "div[class*='ComicViewer'] button[aria-label='Expand comic'] img",
-    );
-    if (comicImages.length !== 1) {
+    const scripts = $('script[type="application/ld+json"]');
+    const desiredDatePublished = date.format("MMMM D, YYYY");
+    const allScriptObjects = scripts
+      .map((_, el): Record<string, any>[] => {
+        const jsonText = $(el).html();
+        if (!jsonText) {
+          return [];
+        }
+        try {
+          const data = JSON.parse(jsonText);
+          // Some pages use an array of objects in a single script tag
+          const entries = Array.isArray(data) ? data : [data];
+          return entries;
+        } catch (err) {
+          console.log(
+            "got error parsing this script as json: ",
+            jsonText,
+            " error was ",
+            err,
+          );
+          return [];
+        }
+      })
+      .toArray()
+      .flat();
+    const entry = allScriptObjects.find((entry: Record<string, any>) => {
+      return (
+        entry.representativeOfPage === true &&
+        entry.datePublished === desiredDatePublished &&
+        entry["@type"] === "ImageObject"
+      );
+    });
+    if (!entry) {
       return scrapeFailure(failureModes.GOCOMICS_MISSING_IMAGE_ON_PAGE);
     }
-    return scrapeSuccess(comicImages.attr("src"));
+    const imageUrl = (entry as Record<string, any>)?.contentUrl;
+    if (!imageUrl) {
+      return scrapeFailure(failureModes.GOCOMICS_MISSING_IMAGE_ON_PAGE);
+    }
+    return scrapeSuccess(imageUrl);
   }
 }
