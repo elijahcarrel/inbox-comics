1b925948c1237cc608f2f0fea4c52a63f690d0a7
Gabe Reiss <gabe@Mac-2884.lan>
2026-05-11 23:20:56 -0700

    fix(gocomics): Use date-specific URL to fix incorrect comic date matching

--- api/src/service/scraper/scrapers/gocomics.ts
+++ api/src/service/scraper/scrapers/gocomics.ts
@@ -1,5 +1,4 @@
 import { Moment } from "moment";
-import moment from "moment-timezone";
 import {
   ISyndication,
   failureModes,
@@ -11,7 +10,6 @@ import {
   scrapeSuccess,
 } from "../common";
 import { Scraper } from "../scraper";
-import sortBy from "lodash/sortBy";
 
 export class GoComicsScraper extends Scraper {
   async scrape(
@@ -19,7 +17,7 @@ export class GoComicsScraper extends Scraper {
     syndication: ISyndication,
   ): Promise<ScrapeResult> {
     const { theiridentifier: theirIdentifier } = syndication;
-    const url = `https://www.gocomics.com/${theirIdentifier}`;
+    const url = `https://www.gocomics.com/${theirIdentifier}/${_date.format("YYYY/MM/DD")}`;
     const $ = await cheerioRequestWithOptions(url, {
       useChromeFingerprint: true,
     });
@@ -50,7 +48,7 @@ export class GoComicsScraper extends Scraper {
       })
       .toArray()
       .flat();
-    const representativeEntries = allScriptObjects.filter(
+    const entry = allScriptObjects.find(
       (entry: Record<string, any>) => {
         return (
           entry.representativeOfPage === true &&
@@ -58,14 +56,6 @@ export class GoComicsScraper extends Scraper {
         );
       },
     );
-    const sortedRepresentativeEntries = sortBy(
-      representativeEntries,
-      (entry: Record<string, any>) => {
-        return moment(entry.datePublished, "MMMM D, YYYY").unix();
-      },
-    );
-    const entry =
-      sortedRepresentativeEntries?.[sortedRepresentativeEntries.length - 1];
     if (!entry) {
       return scrapeFailure(failureModes.GOCOMICS_MISSING_IMAGE_ON_PAGE);
     }
